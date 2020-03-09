53a245a608f63037cdfbf143a06420ea7073817f
Nathaniel Hamming <nate@hmtconsulting.org>
2020-03-09 09:21:18 -0300

    replaced conformance to CustomStringConvertible with toDisplayableString closure

--- LoopKitUI/Views/ListMultipleSelection.swift
+++ LoopKitUI/Views/ListMultipleSelection.swift
@@ -8,28 +8,32 @@
 
 import SwiftUI
 
-public struct ListMultipleSelection<Item>: View where Item: Hashable, Item: CustomStringConvertible {
+public struct ListMultipleSelection<Item>: View where Item: Hashable {
     var items: [Item]
     @Binding var selectedItems: Set<Item>
+    var itemToDisplayableString: (Item) -> String
     
-    public init(items: [Item], selectedItems: Binding<Set<Item>>) {
+    public init(items: [Item], selectedItems: Binding<Set<Item>>, itemToDisplayableString: @escaping (Item) -> String) {
         self.items = items
         _selectedItems = selectedItems
+        self.itemToDisplayableString = itemToDisplayableString
     }
     
     public var body: some View {
         List(items, id:\.self) { item in
-            MultipleSelectionRow<Item>(item: item, selectedItems: self.$selectedItems)
+            MultipleSelectionRow<Item>(item: item,
+                                       selectedItems: self.$selectedItems,
+                                       itemToDisplayableString: self.itemToDisplayableString)
         }
         .listStyle(GroupedListStyle())
         .environment(\.horizontalSizeClass, .regular)
     }
 }
 
-struct MultipleSelectionRow<Item>: View where Item: CustomStringConvertible, Item: Hashable {
+struct MultipleSelectionRow<Item>: View where Item: Hashable {
     var item: Item
-    
     @Binding var selectedItems: Set<Item>
+    var itemToDisplayableString: (Item) -> String
     
     var isSelected: Bool {
         selectedItems.contains(item)
@@ -44,7 +48,7 @@ struct MultipleSelectionRow<Item>: View where Item: CustomStringConvertible, Ite
                     self.selectedItems.insert(self.item)
                 }
             }) {
-                Text(String(describing: item))
+                Text(self.itemToDisplayableString(item))
                     .foregroundColor(.primary)
             }
             Spacer()
@@ -66,7 +70,9 @@ struct SelectableList_Previews: PreviewProvider {
         @State(initialValue: [fruit[1], fruit[3]]) var selectedFruit: Set<String>
         
         var body: some View {
-            ListMultipleSelection<String>(items: SelectableList_Previews.PreviewWrapper.fruit, selectedItems: $selectedFruit)
+            ListMultipleSelection<String>(items: SelectableList_Previews.PreviewWrapper.fruit,
+                                          selectedItems: $selectedFruit,
+                                          itemToDisplayableString: { String(describing:$0) })
         }
     }
 }
