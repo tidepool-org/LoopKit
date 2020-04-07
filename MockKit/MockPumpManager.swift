070f8023fd8ab55daaa53805f67a8a18c6aa48a9
Jamie <jamie.h.bate@gmail.com>
2020-04-07 13:17:50 +1200

    add error for missingSettings, don't default maximumBolus or maximumBasalRatePerHour

--- MockKit/MockPumpManager.swift
+++ MockKit/MockPumpManager.swift
@@ -19,6 +19,7 @@ private enum MockPumpManagerError: LocalizedError {
     case pumpSuspended
     case communicationFailure
     case bolusInProgress
+    case missingSettings
 
     var failureReason: String? {
         switch self {
@@ -28,6 +29,8 @@ private enum MockPumpManagerError: LocalizedError {
             return "Unable to communicate with pump"
         case .bolusInProgress:
             return "Bolus in progress"
+        case .missingSettings:
+            return "Missing Settings."
         }
     }
 }
