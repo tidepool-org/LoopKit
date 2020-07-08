90beb9ffff6b68292ffc05ca4222a723421cc381
Anna <anna@tidepool.org>
2020-07-08 11:18:22 -0700

    Remove \n

--- LoopKitUI/Views/Information Screens/CorrectionRangeOverrideInformationView.swift
+++ LoopKitUI/Views/Information Screens/CorrectionRangeOverrideInformationView.swift
@@ -24,7 +24,7 @@ public struct CorrectionRangeOverrideInformationView: View {
     
     public var body: some View {
         InformationView(
-            title: Text(LocalizedString("Temporary\nCorrection Ranges", comment: "Title for correction range override informational screen")),
+            title: Text(LocalizedString("Temporary Correction Range", comment: "Title for correction range override informational screen")),
             buttonText: Text(LocalizedString("Next: Review Setting", comment: "Button to advance to setting editor")),
             informationalContent: {
                 VStack (alignment: .leading, spacing: 20) {
