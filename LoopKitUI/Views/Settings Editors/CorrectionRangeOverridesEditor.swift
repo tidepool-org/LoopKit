be0ef5c34cd8482bf8d52d5b03e437dd90842749
Rick Pasetto <rick@tidepool.org>
2020-07-10 17:02:09 -0700

    Factor out CorrectionRangeOverridesExpandableSetting

--- LoopKitUI/Views/Settings Editors/CorrectionRangeOverridesEditor.swift
+++ LoopKitUI/Views/Settings Editors/CorrectionRangeOverridesEditor.swift
@@ -104,30 +104,18 @@ public struct CorrectionRangeOverridesEditor: View {
     private func card(for preset: CorrectionRangeOverrides.Preset) -> Card {
         Card {
             SettingDescription(text: description(of: preset), informationalContent: {TherapySetting.correctionRangeOverrides.helpScreen()})
-            ExpandableSetting(
+            CorrectionRangeOverridesExpandableSetting(
                 isEditing: Binding(
                     get: { self.presetBeingEdited == preset },
                     set: { isEditing in
                         withAnimation {
                             self.presetBeingEdited = isEditing ? preset : nil
                         }
-                    }
-                ),
-                leadingValueContent: {
-                    HStack {
-                        icon(for: preset)
-                        name(of: preset)
-                    }
-                },
-                trailingValueContent: {
-                    GuardrailConstrainedQuantityRangeView(
-                        range: value.ranges[preset],
-                        unit: unit,
-                        guardrail: self.guardrail(for: preset),
-                        isEditing: presetBeingEdited == preset,
-                        forceDisableAnimations: true
-                    )
-                },
+                }),
+                value: $value,
+                preset: preset,
+                unit: unit,
+                correctionRangeScheduleRange: correctionRangeScheduleRange,
                 expandedContent: {
                     GlucoseRangePicker(
                         range: Binding(
@@ -136,15 +124,14 @@ public struct CorrectionRangeOverridesEditor: View {
                                 withAnimation {
                                     self.value.ranges[preset] = newValue
                                 }
-                            }
+                        }
                         ),
                         unit: self.unit,
                         minValue: self.selectableBounds(for: preset).lowerBound,
                         maxValue: self.selectableBounds(for: preset).upperBound,
                         guardrail: self.guardrail(for: preset)
                     )
-                }
-            )
+            })
         }
     }
 
@@ -156,32 +143,12 @@ public struct CorrectionRangeOverridesEditor: View {
             return Text("Temporarily raise your glucose target before, during, or after physical activity to reduce the risk of low glucose events.", comment: "Description of workout mode")
         }
     }
-
-    private func name(of preset: CorrectionRangeOverrides.Preset) -> Text {
-        switch preset {
-        case .preMeal:
-            return Text("Pre-Meal", comment: "Title for pre-meal mode configuration section")
-        case .workout:
-            return Text("Workout", comment: "Title for workout mode configuration section")
-        }
-    }
-
-    private func icon(for preset: CorrectionRangeOverrides.Preset) -> some View {
-        switch preset {
-        case .preMeal:
-            return icon(named: "Pre-Meal", tinted: Color(.COBTintColor))
-        case .workout:
-            return icon(named: "workout", tinted: Color(.glucoseTintColor))
-        }
-    }
-
-    private func icon(named name: String, tinted color: Color) -> some View {
-        Image(name)
-            .renderingMode(.template)
-            .foregroundColor(color)
-    }
-
+    
     private func guardrail(for preset: CorrectionRangeOverrides.Preset) -> Guardrail<HKQuantity> {
+        return Self.guardrail(given:  correctionRangeScheduleRange, for: preset)
+    }
+    
+    static func guardrail(given correctionRangeScheduleRange: ClosedRange<HKQuantity>, for preset: CorrectionRangeOverrides.Preset) -> Guardrail<HKQuantity> {
         switch preset {
         case .preMeal:
             return Guardrail(
@@ -195,7 +162,7 @@ public struct CorrectionRangeOverridesEditor: View {
             )
         }
     }
-    
+
     private var buttonText: Text {
         switch mode {
         case .modal:
@@ -288,6 +255,80 @@ public struct CorrectionRangeOverridesEditor: View {
         save(value)
         if mode == .modal {
             dismiss()
+        }
+    }
+}
+
+public struct CorrectionRangeOverridesExpandableSetting<ExpandedContent: View>: View {
+    @Binding var isEditing: Bool
+    @Binding var value: CorrectionRangeOverrides
+    let preset: CorrectionRangeOverrides.Preset
+    let unit: HKUnit
+    var correctionRangeScheduleRange: ClosedRange<HKQuantity>
+    var expandedContent: () -> ExpandedContent
+
+    public var body: some View {
+        ExpandableSetting(
+            isEditing: $isEditing,
+            leadingValueContent: {
+                HStack {
+                    icon(for: preset)
+                    name(of: preset)
+                }
+            },
+            trailingValueContent: {
+                GuardrailConstrainedQuantityRangeView(
+                    range: value.ranges[preset],
+                    unit: unit,
+                    guardrail: self.guardrail(for: preset),
+                    isEditing: isEditing,
+                    forceDisableAnimations: true
+                )
+            },
+            expandedContent: expandedContent
+        )
+    }
+    
+    private func name(of preset: CorrectionRangeOverrides.Preset) -> Text {
+        switch preset {
+        case .preMeal:
+            return Text("Pre-Meal", comment: "Title for pre-meal mode configuration section")
+        case .workout:
+            return Text("Workout", comment: "Title for workout mode configuration section")
+        }
+    }
+
+    private func icon(for preset: CorrectionRangeOverrides.Preset) -> some View {
+        switch preset {
+        case .preMeal:
+            return icon(named: "Pre-Meal", tinted: Color(.COBTintColor))
+        case .workout:
+            return icon(named: "workout", tinted: Color(.glucoseTintColor))
+        }
+    }
+    
+    private func icon(named name: String, tinted color: Color) -> some View {
+        Image(name)
+            .renderingMode(.template)
+            .foregroundColor(color)
+    }
+    
+    private func guardrail(for preset: CorrectionRangeOverrides.Preset) -> Guardrail<HKQuantity> {
+        return Self.guardrail(given:  correctionRangeScheduleRange, for: preset)
+    }
+
+    static func guardrail(given correctionRangeScheduleRange: ClosedRange<HKQuantity>, for preset: CorrectionRangeOverrides.Preset) -> Guardrail<HKQuantity> {
+        switch preset {
+        case .preMeal:
+            return Guardrail(
+                absoluteBounds: Guardrail.correctionRange.absoluteBounds,
+                recommendedBounds: Guardrail.correctionRange.recommendedBounds.lowerBound...max(correctionRangeScheduleRange.lowerBound, Guardrail.correctionRange.recommendedBounds.lowerBound)
+            )
+        case .workout:
+            return Guardrail(
+                absoluteBounds: Guardrail.correctionRange.absoluteBounds,
+                recommendedBounds: max(Guardrail.correctionRange.recommendedBounds.lowerBound, correctionRangeScheduleRange.lowerBound)...Guardrail.correctionRange.absoluteBounds.upperBound
+            )
         }
     }
 }
