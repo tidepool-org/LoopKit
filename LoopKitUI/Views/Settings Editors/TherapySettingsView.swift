e214ca82a94c90504b9d8dbac6d01202b4eac865
Rick Pasetto <rick@tidepool.org>
2020-07-16 14:35:15 -0700

    Tweak

--- LoopKitUI/Views/Settings Editors/TherapySettingsView.swift
+++ LoopKitUI/Views/Settings Editors/TherapySettingsView.swift
@@ -129,34 +129,30 @@ extension TherapySettingsView {
     
     private var correctionRangeSection: some View {
         section(for: .glucoseTargetRange) {
-            Group {
-                if self.glucoseUnit != nil && self.therapySettings.glucoseTargetRangeSchedule != nil {
-                    ForEach(self.therapySettings.glucoseTargetRangeSchedule!.items, id: \.self) { value in
-                        ScheduleRangeItem(time: value.startTime,
-                                          range: value.value,
-                                          unit: self.glucoseUnit!,
-                                          guardrail: .correctionRange)
-                    }
-                } else {
-                    DescriptiveText(label: LocalizedString("Tap \"Edit\" to add a Correction Range", comment: "Correction Range section edit hint"))
+            if self.glucoseUnit != nil && self.therapySettings.glucoseTargetRangeSchedule != nil {
+                ForEach(self.therapySettings.glucoseTargetRangeSchedule!.items, id: \.self) { value in
+                    ScheduleRangeItem(time: value.startTime,
+                                      range: value.value,
+                                      unit: self.glucoseUnit!,
+                                      guardrail: .correctionRange)
                 }
+            } else {
+                DescriptiveText(label: LocalizedString("Tap \"Edit\" to add a Correction Range", comment: "Correction Range section edit hint"))
             }
         }
     }
     
     private var temporaryCorrectionRangesSection: some View {
         section(for: .correctionRangeOverrides) {
-            Group {
-                if self.glucoseUnit != nil && self.therapySettings.glucoseTargetRangeSchedule != nil {
-                    ForEach(CorrectionRangeOverrides.Preset.allCases, id: \.self) { preset in
-                        CorrectionRangeOverridesRangeItem(
-                            preMealTargetRange: self.therapySettings.preMealTargetRange,
-                            workoutTargetRange: self.therapySettings.workoutTargetRange,
-                            unit: self.glucoseUnit!,
-                            preset: preset,
-                            correctionRangeScheduleRange: self.therapySettings.glucoseTargetRangeSchedule!.scheduleRange()
-                        )
-                    }
+            if self.glucoseUnit != nil && self.therapySettings.glucoseTargetRangeSchedule != nil {
+                ForEach(CorrectionRangeOverrides.Preset.allCases, id: \.self) { preset in
+                    CorrectionRangeOverridesRangeItem(
+                        preMealTargetRange: self.therapySettings.preMealTargetRange,
+                        workoutTargetRange: self.therapySettings.workoutTargetRange,
+                        unit: self.glucoseUnit!,
+                        preset: preset,
+                        correctionRangeScheduleRange: self.therapySettings.glucoseTargetRangeSchedule!.scheduleRange()
+                    )
                 }
             }
         }
@@ -164,19 +160,17 @@ extension TherapySettingsView {
     
     private var suspendThresholdSection: some View {
         section(for: .suspendThreshold) {
-            Group {
-                if self.glucoseUnit != nil {
-                    HStack {
-                        Spacer()
-                        GuardrailConstrainedQuantityView(
-                            value: self.therapySettings.suspendThreshold?.quantity,
-                            unit: self.glucoseUnit!,
-                            guardrail: .suspendThreshold,
-                            isEditing: false,
-                            // Workaround for strange animation behavior on appearance
-                            forceDisableAnimations: true
-                        )
-                    }
+            if self.glucoseUnit != nil {
+                HStack {
+                    Spacer()
+                    GuardrailConstrainedQuantityView(
+                        value: self.therapySettings.suspendThreshold?.quantity,
+                        unit: self.glucoseUnit!,
+                        guardrail: .suspendThreshold,
+                        isEditing: false,
+                        // Workaround for strange animation behavior on appearance
+                        forceDisableAnimations: true
+                    )
                 }
             }
         }
@@ -184,14 +178,12 @@ extension TherapySettingsView {
     
     private var basalRatesSection: some View {
         section(for: .basalRate) {
-            Group {
-                if self.therapySettings.basalRateSchedule != nil && self.viewModel.supportedBasalRates != nil {
-                    ForEach(self.therapySettings.basalRateSchedule!.items, id: \.self) { value in
-                        ScheduleValueItem(time: value.startTime,
-                                          value: value.value,
-                                          unit: .internationalUnitsPerHour,
-                                          guardrail: Guardrail.basalRate(supportedBasalRates: self.viewModel.supportedBasalRates!))
-                    }
+            if self.therapySettings.basalRateSchedule != nil && self.viewModel.supportedBasalRates != nil {
+                ForEach(self.therapySettings.basalRateSchedule!.items, id: \.self) { value in
+                    ScheduleValueItem(time: value.startTime,
+                                      value: value.value,
+                                      unit: .internationalUnitsPerHour,
+                                      guardrail: Guardrail.basalRate(supportedBasalRates: self.viewModel.supportedBasalRates!))
                 }
             }
         }
@@ -202,18 +194,16 @@ extension TherapySettingsView {
             HStack {
                 Text(LocalizedString("Maximum Basal Rate", comment: "Maximum Basal Rate settings item title"))
                 Spacer()
-                Group {
-                    if self.viewModel.supportedBasalRates != nil {
-                        
-                        GuardrailConstrainedQuantityView(
-                            value: self.therapySettings.maximumBasalRatePerHour == nil ? nil : HKQuantity(unit: .internationalUnitsPerHour, doubleValue: self.therapySettings.maximumBasalRatePerHour!),
-                            unit: .internationalUnitsPerHour,
-                            guardrail: Guardrail.maximumBasalRate(supportedBasalRates: self.viewModel.supportedBasalRates!, scheduledBasalRange: self.therapySettings.basalRateSchedule?.valueRange()),
-                            isEditing: false,
-                            // Workaround for strange animation behavior on appearance
-                            forceDisableAnimations: true
-                        )
-                    }
+                if self.viewModel.supportedBasalRates != nil {
+                    
+                    GuardrailConstrainedQuantityView(
+                        value: self.therapySettings.maximumBasalRatePerHour == nil ? nil : HKQuantity(unit: .internationalUnitsPerHour, doubleValue: self.therapySettings.maximumBasalRatePerHour!),
+                        unit: .internationalUnitsPerHour,
+                        guardrail: Guardrail.maximumBasalRate(supportedBasalRates: self.viewModel.supportedBasalRates!, scheduledBasalRange: self.therapySettings.basalRateSchedule?.valueRange()),
+                        isEditing: false,
+                        // Workaround for strange animation behavior on appearance
+                        forceDisableAnimations: true
+                    )
                 }
             }
         }
@@ -235,7 +225,7 @@ extension TherapySettingsView {
         glucoseUnit?.unitDivided(by: .internationalUnit())
     }
 
-    private func section<Content>(for therapySetting: TherapySetting, content: @escaping () -> Content) -> some View where Content: View {
+    private func section<Content>(for therapySetting: TherapySetting, @ViewBuilder content: @escaping () -> Content) -> some View where Content: View {
         SectionWithEdit(isEditing: $isEditing,
                         title: therapySetting.title,
                         descriptiveText: therapySetting.descriptiveText,
