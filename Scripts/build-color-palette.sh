6f4d5b23d7fad3c505cc0a33c31d4716c3d942d9
Nathaniel Hamming <nate@hmtconsulting.org>
2020-07-17 08:28:07 -0300

    renaming color palette items to match the existing pattern

--- Scripts/build-color-palette.sh
+++ Scripts/build-color-palette.sh
@@ -36,18 +36,18 @@ if [ ! -d "${DIRECTORY}" ]; then
     error "Directory '${DIRECTORY}' does not exist"
 fi
 
-COLOR_PALETTE_DERIVED="${DIRECTORY}/ColorPaletteDerived.xcassets"
-COLOR_PALETTE_BASE="${DIRECTORY}/ColorPaletteBase.xcassets"
-COLOR_PALETTE_OVERRIDE="${DIRECTORY}/ColorPaletteOverride.xcassets"
+COLOR_PALETTE_DERIVED="${DIRECTORY}/DerivedColorPalette.xcassets"
+COLOR_PALETTE_BASE="${DIRECTORY}/DerivedColorPaletteBase.xcassets"
+COLOR_PALETTE_OVERRIDE="${DIRECTORY}/DerivedColorPaletteOverride.xcassets"
 
-info "Building color palette for ${DIRECTORY}..."
+info "Removing derived color palette for ${DIRECTORY}..."
 rm -rf "${COLOR_PALETTE_DERIVED}"
 
 info "Copying color palette base to the derived color palette..."
 cp -av "${COLOR_PALETTE_BASE}" "${COLOR_PALETTE_DERIVED}"
 
 if [ -e "${COLOR_PALETTE_OVERRIDE}" ]; then
-  info "Copying color palette override to color palette..."
+  info "Copying color palette override to the derived color palette..."
   for ASSET_PATH in "${COLOR_PALETTE_OVERRIDE}"/*; do
     ASSET_FILE="$(basename "${ASSET_PATH}")"
     rm -rf "${COLOR_PALETTE_DERIVED}/${ASSET_FILE}"
