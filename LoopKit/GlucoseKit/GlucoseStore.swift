7b7400c51fa1059db2263c2d7eccdd78a9228d83
Anna <anna@tidepool.org>
2020-08-07 13:02:31 -0700

    Add fields to glucose store protocol

--- LoopKit/GlucoseKit/GlucoseStore.swift
+++ LoopKit/GlucoseKit/GlucoseStore.swift
@@ -28,8 +28,45 @@ public enum GlucoseStoreResult<T> {
 }
 
 public protocol GlucoseStoreProtocol {
+    
+    var latestGlucose: GlucoseSampleValue? { get }
+    
+    var preferredUnit: HKUnit? { get }
+    
+    var sampleType: HKSampleType { get }
+    
+    var delegate: GlucoseStoreDelegate? { get set }
+    
+    var managedDataInterval: TimeInterval? { get set }
+    
+    /// MARK: HealthKit
+    var authorizationRequired: Bool { get }
+    
+    var sharingDenied: Bool { get }
+    
+    var healthStore: HKHealthStore { get }
+    
+    func authorize(toShare: Bool, _ completion: @escaping (_ result: HealthKitSampleStoreResult<Bool>) -> Void)
+    
+    /// MARK: sample management
+    func addGlucose(_ glucose: NewGlucoseSample, completion: @escaping (_ result: GlucoseStoreResult<GlucoseValue>) -> Void)
+    
+    func addGlucose(_ values: [NewGlucoseSample], completion: @escaping (_ result: GlucoseStoreResult<[GlucoseValue]>) -> Void)
+    
+    func getCachedGlucoseSamples(start: Date, end: Date?, completion: @escaping (_ samples: [StoredGlucoseSample]) -> Void)
+    
+    func generateDiagnosticReport(_ completion: @escaping (_ report: String) -> Void)
+    
+    func purgeGlucoseSamples(matchingCachePredicate cachePredicate: NSPredicate?, healthKitPredicate: NSPredicate, completion: @escaping (_ success: Bool, _ count: Int, _ error: Error?) -> Void)
+    
+    func executeGlucoseQuery(fromQueryAnchor queryAnchor: GlucoseStore.QueryAnchor?, limit: Int, completion: @escaping (GlucoseStore.GlucoseQueryResult) -> Void)
+    
+    /// MARK: effect calculation
     func getRecentMomentumEffect(_ completion: @escaping (_ effects: [GlucoseEffect]) -> Void)
+    
     func getCounteractionEffects(start: Date, end: Date?, to effects: [GlucoseEffect], _ completion: @escaping (_ effects: [GlucoseEffectVelocity]) -> Void)
+    
+    func counteractionEffects<Sample: GlucoseSampleValue>(for samples: [Sample], to effects: [GlucoseEffect]) -> [GlucoseEffectVelocity]
 }
 
 /**
