21b6ab12f1a64b37c38b3742fd61c2914da79012
Nathaniel Hamming <nate@hmtconsulting.org>
2020-09-11 08:45:48 -0300

    response to comments

--- LoopKitUI/HorizontalSizeClassOverride.swift
+++ LoopKitUI/HorizontalSizeClassOverride.swift
@@ -13,8 +13,19 @@ public protocol HorizontalSizeClassOverride {
 }
 
 public extension HorizontalSizeClassOverride {
+    // iPod Touch and iPhone SE 1st generation are considered to have a short display
+    static var shortDisplayLimit: CGFloat {
+        return 640
+    }
+    
+    // iPod Touch and iPhone SE 1st Generation are considered to have a narrow display
+    static var narrowDisplayLimit: CGFloat {
+        return 370
+    }
+    
+    
     var horizontalOverride: UserInterfaceSizeClass {
-        if UIScreen.main.bounds.height <= 640 {
+        if UIScreen.main.bounds.height <= Self.shortDisplayLimit {
             return .compact
         } else {
             return .regular
@@ -22,7 +33,7 @@ public extension HorizontalSizeClassOverride {
     }
         
     var isDisplayNarrow: Bool {
-        if UIScreen.main.bounds.width < 375 {
+        if UIScreen.main.bounds.width <= Self.narrowDisplayLimit {
             return true
         } else {
             return false
