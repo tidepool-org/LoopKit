cd1b44de685fded31a8690ecac12c2c584bbb657
Rick Pasetto <rick@tidepool.org>
2020-11-16 14:49:16 -0800

    Add illustration to "Glucose Safety Limit" (f.k.a. "Suspend Threshold")

--- LoopKitUI/Views/Information Screens/SuspendThresholdInformationView.swift
+++ LoopKitUI/Views/Information Screens/SuspendThresholdInformationView.swift
@@ -6,12 +6,14 @@
 //  Copyright © 2020 LoopKit Authors. All rights reserved.
 //
 
+import HealthKit
 import SwiftUI
 import LoopKit
 
 public struct SuspendThresholdInformationView: View {
     var onExit: (() -> Void)?
     var mode: PresentationMode
+    var preferredUnit: HKUnit = HKUnit.milligramsPerDeciliter
     
     @Environment(\.presentationMode) var presentationMode
     
@@ -26,10 +28,22 @@ public struct SuspendThresholdInformationView: View {
     public var body: some View {
         InformationView(
             title: Text(TherapySetting.suspendThreshold.title),
-            informationalContent: {text},
+            informationalContent: {
+                VStack {
+                    illustration
+                    text
+                }
+            },
             onExit: onExit ?? { self.presentationMode.wrappedValue.dismiss() },
             mode: mode
         )
+    }
+    
+    private var illustration: some View {
+        Image(frameworkImage: illustrationImageName)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: ContentMode.fit)
     }
     
     private var text: some View {
@@ -38,5 +52,34 @@ public struct SuspendThresholdInformationView: View {
         }
         .accentColor(.secondary)
         .foregroundColor(.accentColor)
+    }
+    
+    private var illustrationImageName: String {
+        switch preferredUnit {
+        case .milligramsPerDeciliter:
+            return "Glucose Safety Limit mgdL"
+        case .millimolesPerLiter:
+            return "Glucose Safety Limit mmolL"
+        default:
+            fatalError()
+        }
+    }
+}
+
+struct SuspendThresholdInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            SuspendThresholdInformationView(onExit: nil, mode: .acceptanceFlow)
+                .colorScheme(.light)
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE 2"))
+                .previewDisplayName("SE light")
+        }
+        NavigationView {
+            SuspendThresholdInformationView(onExit: nil, mode: .acceptanceFlow)
+            .preferredColorScheme(.dark)
+            .colorScheme(.dark)
+            .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
+            .previewDisplayName("11 Pro dark")
+        }
     }
 }
