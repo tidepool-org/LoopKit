a2cd50e3c6b48dad223fa6b591cb9740a79efe70
Rick Pasetto <rick@tidepool.org>
2020-11-16 15:06:45 -0800

    Basal rate illustration

--- LoopKitUI/Views/Information Screens/BasalRatesInformationView.swift
+++ LoopKitUI/Views/Information Screens/BasalRatesInformationView.swift
@@ -6,13 +6,15 @@
 //  Copyright © 2020 LoopKit Authors. All rights reserved.
 //
 
+import HealthKit
 import SwiftUI
 import LoopKit
 
 public struct BasalRatesInformationView: View {
     var onExit: (() -> Void)?
     var mode: PresentationMode
-    
+    var preferredUnit: HKUnit = HKUnit.milligramsPerDeciliter
+
     @Environment(\.presentationMode) var presentationMode
     
     public init(onExit: (() -> Void)?, mode: PresentationMode = .acceptanceFlow) {
@@ -23,10 +25,22 @@ public struct BasalRatesInformationView: View {
     public var body: some View {
         InformationView(
             title: Text(TherapySetting.basalRate.title),
-            informationalContent: {text},
+            informationalContent: {
+                VStack {
+                    illustration
+                    text
+                }
+            },
             onExit: onExit ?? { self.presentationMode.wrappedValue.dismiss() },
             mode: mode
         )
+    }
+    
+    private var illustration: some View {
+        Image(frameworkImage: illustrationImageName)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: ContentMode.fit)
     }
     
     private var text: some View {
@@ -36,5 +50,35 @@ public struct BasalRatesInformationView: View {
             Text(LocalizedString("The schedule starts at midnight and cannot contain a rate of 0 U/hr.", comment: "Information about basal rate scheduling"))
         }
         .foregroundColor(.secondary)
+    }
+    
+    private var illustrationImageName: String {
+        switch preferredUnit {
+        case .milligramsPerDeciliter:
+            return "Correction Range mgdL"
+        case .millimolesPerLiter:
+            return "Correction Range mmolL"
+        default:
+            fatalError()
+        }
+    }
+
+}
+
+struct BasalRatesInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            BasalRatesInformationView(onExit: nil, mode: .acceptanceFlow)
+                .colorScheme(.light)
+                .previewDevice(PreviewDevice(rawValue: "iPhone SE 2"))
+                .previewDisplayName("SE light")
+        }
+        NavigationView {
+            BasalRatesInformationView(onExit: nil, mode: .acceptanceFlow)
+            .preferredColorScheme(.dark)
+            .colorScheme(.dark)
+            .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro Max"))
+            .previewDisplayName("11 Pro dark")
+        }
     }
 }
