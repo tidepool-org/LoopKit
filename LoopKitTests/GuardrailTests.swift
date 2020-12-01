2298fdf249170bd12f56a7dbd46b09cd38790687
Rick Pasetto <rick@tidepool.org>
2020-12-01 14:30:56 -0800

    LOOP-2479, LOOP-2480: Make sure max basal rate guardrail absolute bounds don't cross
    With the new rule that the max basal rate absolute bounds is:
    
    Maximum:  70 / Lowest Carb Ratio (in the user’s carb ratio schedule)
    Minimum:   Highest Scheduled Basal Rate
    
    Should also address LOOP-2480

--- LoopKitTests/GuardrailTests.swift
+++ LoopKitTests/GuardrailTests.swift
@@ -158,6 +158,15 @@ class GuardrailTests: XCTestCase {
         XCTAssertEqual(guardrail.recommendedBounds.range(withUnit: .internationalUnitsPerHour), 1.6...5.0)
     }
     
+    func testMaxBasalRateGuardrailHighCarbRatio() {
+        let supportedBasalRates = (1...600).map { Double($0) / 20 }
+        let scheduledBasalRange = 0.05...0.78125
+        let lowestCarbRatio = 150.0
+        let guardrail = Guardrail.maximumBasalRate(supportedBasalRates: supportedBasalRates, scheduledBasalRange: scheduledBasalRange, lowestCarbRatio: lowestCarbRatio)
+        XCTAssertEqual(guardrail.absoluteBounds.range(withUnit: .internationalUnitsPerHour), 0.78125...0.78125)
+        XCTAssertEqual(guardrail.recommendedBounds.range(withUnit: .internationalUnitsPerHour), 0.78125...0.78125)
+    }
+    
     func testMaxBasalRateGuardrailHigherCarbRatioClampsRecommendedBounds() {
         let supportedBasalRates = (1...600).map { Double($0) / 20 }
         let scheduledBasalRange = 0.05...0.78125
