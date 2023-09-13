6b652cf24fd9455fac0fe2a50a16d474e088b58a
Nathaniel Hamming <nate@hmtconsulting.org>
2023-09-13 10:47:59 -0300

    updated to security protocol

--- LoopKit/Security/Security.swift
+++ LoopKit/Security/Security.swift
@@ -8,8 +8,17 @@
 
 import Foundation
 
+public protocol SecurityDelegate: AnyObject {
+    /// Informs the delegate that the state of the specified security was updated and the delegate should persist the security. May
+    /// be invoked prior to the security completing setup.
+    ///
+    /// - Parameters:
+    ///     - security: The security that updated state.
+    func securityDidUpdateState(_ security: Security)
+}
+
 public protocol SecurityPlugin {
-    var security: Security { get }
+    var securityType: Security.Type? { get }
 }
 
 public protocol SecurityProvider {
@@ -22,6 +31,20 @@ public protocol SecurityProvider {
 }
 
 public protocol Security: Pluggable {
-    func verifyDevice(completion: @escaping (Result<Bool, Error>) -> Void)
-    func verifyApp(completion: @escaping (Result<Bool, Error>) -> Void)
+    typealias RawStateValue = [String: Any]
+    
+    /// The delegate to notify of security updates.
+    var delegate: SecurityDelegate? { get set }
+    
+    /// Initializes the security with the default state.
+    init()
+    
+    /// Initializes the security with the previously-serialized state.
+    ///
+    /// - Parameters:
+    ///     - rawState: The previously-serialized state of the security.
+    init?(rawState: RawStateValue)
+
+    /// The current, serializable state of the security.
+    var rawState: RawStateValue { get }
 }
