201d68eec23b4ce3d7ec763a0c7bf9d1dfbf0c67
Pete Schwamb <pete@schwamb.net>
2024-01-16 12:35:56 -0600

    LoopKit compiling with LoopAlgorithm swift package

--- LoopKitUI/Views/Settings Editors/InsulinModelSelection.swift
+++ LoopKitUI/Views/Settings Editors/InsulinModelSelection.swift
@@ -199,9 +199,15 @@ public struct InsulinModelSelection: View {
     }
     
     private func oneUnitBolusEffectPrediction(using modelPreset: ExponentialInsulinModelPreset) -> [GlucoseValue] {
-        let bolus = DoseEntry(type: .bolus, startDate: chartManager.startDate, value: 1, unit: .units, insulinType: .novolog)
+        let bolus = BasalRelativeDose(type: .bolus, startDate: chartManager.startDate, endDate: chartManager.startDate, volume: 1, insulinType: .novolog)
         let startingGlucoseSample = HKQuantitySample(type: HealthKitSampleStore.glucoseType, quantity: startingGlucoseQuantity, start: chartManager.startDate, end: chartManager.startDate)
-        let effects = [bolus].glucoseEffects(insulinModelProvider: StaticInsulinModelProvider(modelPreset), longestEffectDuration: .hours(6), insulinSensitivity: insulinSensitivitySchedule)
+        let isfTimeline = insulinSensitivitySchedule.quantitiesBetween(
+            start: chartManager.startDate,
+            end: chartManager.startDate.addingTimeInterval(
+                InsulinMath.longestInsulinActivityDuration
+            )
+        )
+        let effects = [bolus].glucoseEffects(insulinModelProvider: StaticInsulinModelProvider(modelPreset), longestEffectDuration: .hours(6), insulinSensitivityTimeline: isfTimeline)
         return LoopMath.predictGlucose(startingAt: startingGlucoseSample, effects: effects)
     }
 
