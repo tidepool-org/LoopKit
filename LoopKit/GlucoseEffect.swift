6e43cf96c87115703183874c6d10d5d103c240c9
Pete Schwamb <pete@schwamb.net>
2024-02-06 11:58:56 -0600

    More cleanup

--- LoopKit/GlucoseEffect.swift
+++ LoopKit/GlucoseEffect.swift
@@ -10,44 +10,6 @@ import Foundation
 import HealthKit
 import LoopAlgorithm
 
-//
-//public struct GlucoseEffect: GlucoseValue, Equatable {
-//    public let startDate: Date
-//    public let quantity: HKQuantity
-//
-//    public init(startDate: Date, quantity: HKQuantity) {
-//        self.startDate = startDate
-//        self.quantity = quantity
-//    }
-//}
-//
-//extension GlucoseEffect: Codable {
-//    public init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        self.init(startDate: try container.decode(Date.self, forKey: .startDate),
-//                  quantity: HKQuantity(unit: .milligramsPerDeciliter, doubleValue: try container.decode(Double.self, forKey: .quantity)))
-//    }
-//
-//    public func encode(to encoder: Encoder) throws {
-//        var container = encoder.container(keyedBy: CodingKeys.self)
-//        try container.encode(startDate, forKey: .startDate)
-//        try container.encode(quantity.doubleValue(for: .milligramsPerDeciliter), forKey: .quantity)
-//    }
-//
-//    private enum CodingKeys: String, CodingKey {
-//        case startDate
-//        case quantity
-//    }
-//}
-//
-//
-//extension GlucoseEffect: Comparable {
-//    public static func <(lhs: GlucoseEffect, rhs: GlucoseEffect) -> Bool {
-//        return lhs.startDate < rhs.startDate
-//    }
-//}
-//
-
 public extension Array where Element == GlucoseEffect {
     func asVelocities() -> [GlucoseEffectVelocity] {
         guard count > 1 else {
--- LoopKit/GlucoseRangeSchedule.swift
+++ LoopKit/GlucoseRangeSchedule.swift
@@ -10,48 +10,6 @@ import Foundation
 import HealthKit
 import LoopAlgorithm
 
-//public struct DoubleRange {
-//    public let minValue: Double
-//    public let maxValue: Double
-//
-//    public init(minValue: Double, maxValue: Double) {
-//        self.minValue = minValue
-//        self.maxValue = maxValue
-//    }
-//
-//    public var isZero: Bool {
-//        return abs(minValue) < .ulpOfOne && abs(maxValue) < .ulpOfOne
-//    }
-//}
-//
-//
-//extension DoubleRange: RawRepresentable {
-//    public typealias RawValue = [Double]
-//
-//    public init?(rawValue: RawValue) {
-//        guard rawValue.count == 2 else {
-//            return nil
-//        }
-//
-//        minValue = rawValue[0]
-//        maxValue = rawValue[1]
-//    }
-//
-//    public var rawValue: RawValue {
-//        return [minValue, maxValue]
-//    }
-//}
-//
-//extension DoubleRange: Equatable {
-//    public static func ==(lhs: DoubleRange, rhs: DoubleRange) -> Bool {
-//        return abs(lhs.minValue - rhs.minValue) < .ulpOfOne &&
-//               abs(lhs.maxValue - rhs.maxValue) < .ulpOfOne
-//    }
-//}
-//
-//extension DoubleRange: Hashable {}
-//
-//extension DoubleRange: Codable {}
 
 public typealias GlucoseRangeTimeline = [AbsoluteScheduleValue<ClosedRange<HKQuantity>>]
 
@@ -232,14 +190,6 @@ extension ClosedRange where Bound == HKQuantity {
     }
 }
 
-//extension DoubleRange {
-//    public func quantityRange(for unit: HKUnit) -> ClosedRange<HKQuantity> {
-//        let lowerBound = HKQuantity(unit: unit, doubleValue: minValue)
-//        let upperBound = HKQuantity(unit: unit, doubleValue: maxValue)
-//        return lowerBound...upperBound
-//    }
-//}
-//
 extension ClosedRange where Bound == HKQuantity {
     public func doubleRange(for unit: HKUnit) -> DoubleRange {
         return DoubleRange(minValue: lowerBound.doubleValue(for: unit), maxValue: upperBound.doubleValue(for: unit))
