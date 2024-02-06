97ca3e5e0e2ea044daaa83e9ace968f7dc6cb759
Pete Schwamb <pete@schwamb.net>
2024-02-06 12:03:43 -0600

    More cleanup

--- LoopKit/DailyValueSchedule.swift
+++ LoopKit/DailyValueSchedule.swift
@@ -33,20 +33,6 @@ extension RepeatingScheduleValue: Equatable where T: Equatable {
 
 extension RepeatingScheduleValue: Hashable where T: Hashable {}
 
-//public struct AbsoluteScheduleValue<T>: TimelineValue {
-//    public let startDate: Date
-//    public let endDate: Date
-//    public let value: T
-//
-//    public init(startDate: Date, endDate: Date, value: T) {
-//        self.startDate = startDate
-//        self.endDate = endDate
-//        self.value = value
-//    }
-//}
-//
-//extension AbsoluteScheduleValue: Equatable where T: Equatable {}
-
 extension RepeatingScheduleValue: RawRepresentable where T: RawRepresentable {
     public typealias RawValue = [String: Any]
 
