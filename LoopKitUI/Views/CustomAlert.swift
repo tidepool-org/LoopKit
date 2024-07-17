65984c73f41fe5c1e9c59cce3a149ac69d0210db
Cameron Ingham <camji555@gmail.com>
2024-07-17 12:03:02 -0700

    [COASTAL-1411] Add CustomAlert with image support

--- LoopKitUI/Views/CustomAlert.swift
+++ LoopKitUI/Views/CustomAlert.swift
@@ -8,53 +8,49 @@
 
 import SwiftUI
 
-public struct CustomAlertPresenter: UIViewControllerRepresentable {
-    public struct CustomAlert {
-        public struct AlertAction {
-            let title: String?
-            let style: UIAlertAction.Style
-            let handler: ((UIAlertAction) -> Void)
-            
-            public init(title: String?, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)? = nil) {
-                self.title = title
-                self.style = style
-                self.handler = handler ?? { _ in }
-            }
-        }
-        
-        let image: UIImage?
-        let imageBounds: (NSTextAttachment) -> CGRect
-        let title: String?
-        let message: String?
-        let primaryAction: AlertAction?
-        let secondaryAction: AlertAction?
-        
-        public init(
-            image: UIImage?,
-            imageBounds: @escaping (NSTextAttachment) -> CGRect = { titleImageAttachment in
-                CGRect(x: titleImageAttachment.bounds.origin.x, y: -10, width: 40, height: 35)
-            },
-            title: String?,
-            message: String?,
-            primaryAction: AlertAction?,
-            secondaryAction: AlertAction?
-        ) {
-            self.image = image
-            self.imageBounds = imageBounds
-            self.title = title
-            self.message = message
-            self.primaryAction = primaryAction
-            self.secondaryAction = secondaryAction
-        }
+public struct CustomAlert {
+    public enum ImageOption {
+        case warning
+        case custom(
+            UIImage?,
+            bounds: (NSTextAttachment) -> CGRect
+        )
     }
+    
+    let image: ImageOption?
+    let title: String?
+    let message: String?
+    let primaryAction: UIAlertAction?
+    let secondaryAction: UIAlertAction?
+    
+    public init(
+        image: ImageOption?,
+        title: String?,
+        message: String?,
+        primaryAction: UIAlertAction?,
+        secondaryAction: UIAlertAction?
+    ) {
+        self.image = image
+        self.title = title
+        self.message = message
+        self.primaryAction = primaryAction
+        self.secondaryAction = secondaryAction
+    }
+}
+
+public struct CustomAlertPresenter: UIViewControllerRepresentable {
+    
+    @Environment(\.guidanceColors) private var guidanceColors
     
     public class CustomAlertPresentingViewController: UIViewController {
         private var alertController: UIAlertController?
         
         private let alertBinding: Binding<CustomAlert?>
+        private let guidanceColors: GuidanceColors
         
-        public init(alertBinding: Binding<CustomAlert?>) {
+        fileprivate init(alertBinding: Binding<CustomAlert?>, guidanceColors: GuidanceColors) {
             self.alertBinding = alertBinding
+            self.guidanceColors = guidanceColors
             super.init(nibName: nil, bundle: nil)
         }
         
@@ -67,27 +63,40 @@ public struct CustomAlertPresenter: UIViewControllerRepresentable {
                 if let alert {
                     alertController = UIAlertController(title: alert.title, message: alert.message, preferredStyle: .alert)
                     
-                    if let title = alert.title {
+                    if let title = alert.title, let image = alert.image {
                         let titleImageAttachment = NSTextAttachment()
-                        titleImageAttachment.image = alert.image
-                        titleImageAttachment.bounds = alert.imageBounds(titleImageAttachment)
+                        switch image {
+                        case .warning:
+                            titleImageAttachment.image = UIImage(systemName: "exclamationmark.triangle.fill")?.withTintColor(UIColor(guidanceColors.critical))
+                            titleImageAttachment.bounds = CGRect(x: titleImageAttachment.bounds.origin.x, y: -10, width: 40, height: 35)
+                        case let .custom(uIImage, bounds):
+                            titleImageAttachment.image = uIImage
+                            titleImageAttachment.bounds = bounds(titleImageAttachment)
+                        }
+                        
                         let titleWithImage = NSMutableAttributedString(attachment: titleImageAttachment)
                         titleWithImage.append(NSMutableAttributedString(string: "\n\n", attributes: [.font: UIFont.systemFont(ofSize: 8)]))
                         titleWithImage.append(NSMutableAttributedString(string: title, attributes: [.font: UIFont.preferredFont(forTextStyle: .headline)]))
                         alertController?.setValue(titleWithImage, forKey: "attributedTitle")
                     }
                     
+                    typealias AlertHandler = @convention(block) (UIAlertAction) -> Void
+                    
                     if let primaryAction = alert.primaryAction {
                         alertController?.addAction(UIAlertAction(title: primaryAction.title, style: primaryAction.style, handler: { [weak self] action in
                             self?.alertBinding.wrappedValue = nil
-                            primaryAction.handler(action)
+                            let block = primaryAction.value(forKey: "handler")
+                            let handler = unsafeBitCast(block as AnyObject, to: AlertHandler.self)
+                            handler(action)
                         }))
                     }
                     
                     if let secondaryAction = alert.secondaryAction {
                         alertController?.addAction(UIAlertAction(title: secondaryAction.title, style: secondaryAction.style, handler: { [weak self] action in
                             self?.alertBinding.wrappedValue = nil
-                            secondaryAction.handler(action)
+                            let block = secondaryAction.value(forKey: "handler")
+                            let handler = unsafeBitCast(block as AnyObject, to: AlertHandler.self)
+                            handler(action)
                         }))
                     }
                     
@@ -104,7 +113,7 @@ public struct CustomAlertPresenter: UIViewControllerRepresentable {
     @Binding fileprivate var alert: CustomAlert?
     
     public func makeUIViewController(context: Context) -> CustomAlertPresentingViewController {
-        CustomAlertPresentingViewController(alertBinding: $alert)
+        CustomAlertPresentingViewController(alertBinding: $alert, guidanceColors: guidanceColors)
     }
     
     public func updateUIViewController(_ uiViewController: CustomAlertPresentingViewController, context: Context) {
@@ -114,7 +123,7 @@ public struct CustomAlertPresenter: UIViewControllerRepresentable {
 
 public extension View {
     @ViewBuilder
-    func customAlert(item: Binding<CustomAlertPresenter.CustomAlert?>) -> some View {
+    func customAlert(item: Binding<CustomAlert?>) -> some View {
         self.background(CustomAlertPresenter(alert: item))
     }
 }
