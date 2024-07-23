5f7d3a6b4ff4aebfee2c6c382b9c234f4a10b3b9
Nathaniel Hamming <nate@hmtconsulting.org>
2024-07-23 06:37:07 -0300

    Updated migration test

--- LoopKitHostedTests/CoreDataMigrationTests.swift
+++ LoopKitHostedTests/CoreDataMigrationTests.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2021 LoopKit Authors. All rights reserved.
 //
 
+// based on https://ifcaselet.com/writing-unit-tests-for-core-data-migrations/
+
 import CoreData
 import HealthKit
 import XCTest
@@ -13,10 +15,30 @@ import XCTest
 
 class CoreDataMigrationTests: XCTestCase {
     
-    private let momdURL = Bundle(for: CoreDataMigrationTests.self).url(forResource: "App", withExtension: "momd")!
+    private let momdURL = Bundle(for: PersistenceController.self).url(forResource: "Model", withExtension: "momd")!
     private let storeType = NSSQLiteStoreType
-
     
+    func testV4toV5Migration() throws {
+        // create model V4
+        let modelV4Container = try startPersistentContainer("Modelv4")
+        
+        let modelV4Description = NSEntityDescription.entity(forEntityName: "CachedInsulinDeliveryObject", in: modelV4Container.viewContext)!
+        XCTAssertTrue(modelV4Description.propertiesByName.keys.contains("value"))
+        XCTAssertFalse(modelV4Description.propertiesByName.keys.contains("deliveredUnits"))
+        XCTAssertFalse(modelV4Description.propertiesByName.keys.contains("programmedUnits"))
+        
+        // migrate V4 -> V5
+        let modelV5Container = try migrate(container: modelV4Container, to: "Modelv5")
+        
+        let modelV5Description = NSEntityDescription.entity(forEntityName: "CachedInsulinDeliveryObject", in: modelV5Container.viewContext)!
+        XCTAssertFalse(modelV5Description.propertiesByName.keys.contains("value"))
+        XCTAssertTrue(modelV5Description.propertiesByName.keys.contains("deliveredUnits"))
+        XCTAssertTrue(modelV5Description.propertiesByName.keys.contains("programmedUnits"))
+    }
+}
+    
+// taken from https://ifcaselet.com/writing-unit-tests-for-core-data-migrations/
+extension CoreDataMigrationTests {
     /// Create and load a store using the given model version. The store will be located in a
     /// temporary directory.
     ///
@@ -25,16 +47,16 @@ class CoreDataMigrationTests: XCTestCase {
     func startPersistentContainer(_ versionName: String) throws -> NSPersistentContainer {
         let storeURL = makeTemporaryStoreURL()
         let model = managedObjectModel(versionName: versionName)
-
+        
         let container = makePersistentContainer(storeURL: storeURL,
                                                 managedObjectModel: model)
         container.loadPersistentStores { _, error in
             XCTAssertNil(error)
         }
-
+        
         return container
     }
-
+    
     /// Migrates the given `container` to a new store URL. The new (migrated) store will be located
     /// in a temporary directory.
     ///
@@ -49,17 +71,17 @@ class CoreDataMigrationTests: XCTestCase {
         // Define the source and destination `NSManagedObjectModels`.
         let sourceModel = container.managedObjectModel
         let destinationModel = managedObjectModel(versionName: versionName)
-
+        
         let sourceStoreURL = storeURL(from: container)
         // Create a new temporary store URL. This is where the migrated data using the model
         // will be located.
         let destinationStoreURL = makeTemporaryStoreURL()
-
+        
         // Infer a mapping model between the source and destination `NSManagedObjectModels`.
         // Modify this line if you use a custom mapping model.
         let mappingModel = try NSMappingModel.inferredMappingModel(forSourceModel: sourceModel,
                                                                    destinationModel: destinationModel)
-
+        
         let migrationManager = NSMigrationManager(sourceModel: sourceModel,
                                                   destinationModel: destinationModel)
         // Migrate the `sourceStoreURL` to `destinationStoreURL`.
@@ -70,17 +92,17 @@ class CoreDataMigrationTests: XCTestCase {
                                           toDestinationURL: destinationStoreURL,
                                           destinationType: storeType,
                                           destinationOptions: nil)
-
+        
         // Load the store at `destinationStoreURL` and return the migrated container.
         let destinationContainer = makePersistentContainer(storeURL: destinationStoreURL,
                                                            managedObjectModel: destinationModel)
         destinationContainer.loadPersistentStores { _, error in
             XCTAssertNil(error)
         }
-
+        
         return destinationContainer
     }
-
+    
     private func makePersistentContainer(storeURL: URL,
                                          managedObjectModel: NSManagedObjectModel) -> NSPersistentContainer {
         let description = NSPersistentStoreDescription(url: storeURL)
@@ -89,97 +111,26 @@ class CoreDataMigrationTests: XCTestCase {
         // from **automatically** migrating the store. Leaving this as `true` might result in false positives.
         description.shouldMigrateStoreAutomatically = false
         description.type = storeType
-
+        
         let container = NSPersistentContainer(name: "App Container", managedObjectModel: managedObjectModel)
         container.persistentStoreDescriptions = [description]
-
+        
         return container
     }
-
+    
     private func managedObjectModel(versionName: String) -> NSManagedObjectModel {
         let url = momdURL.appendingPathComponent(versionName).appendingPathExtension("mom")
         return NSManagedObjectModel(contentsOf: url)!
     }
-
+    
     private func storeURL(from container: NSPersistentContainer) -> URL {
         let description = container.persistentStoreDescriptions.first!
         return description.url!
     }
-
+    
     private func makeTemporaryStoreURL() -> URL {
         URL(fileURLWithPath: NSTemporaryDirectory())
             .appendingPathComponent(UUID().uuidString)
             .appendingPathExtension("sqlite")
-    }
-    
-    // ---- original ----
-    
-    var cacheStore: PersistenceController!
-    let fileManager = FileManager()
-    static let bundleURL = Bundle(for: CoreDataMigrationTests.self).bundleURL
-
-    override func setUpWithError() throws {
-        try? fileManager.removeItem(at: Self.bundleURL.appendingPathComponent("Model.sqlite"))
-        try fileManager.copyItem(at: Self.bundleURL.appendingPathComponent("Model.sqlite.v4.original"),
-                                 to: Self.bundleURL.appendingPathComponent("Model.sqlite"))
-    }
-
-    override func tearDownWithError() throws {
-        
-        // remove all stores
-        try cacheStore.managedObjectContext.persistentStoreCoordinator?.persistentStores.forEach { store in
-            try cacheStore.managedObjectContext.persistentStoreCoordinator?.remove(store)
-        }
-        
-        try fileManager.removeItem(at: Self.bundleURL.appendingPathComponent("Model.sqlite"))
-        cacheStore = nil
-    }
-
-    func testV4toV5Migration() throws {
-        let e = expectation(description: "\(#function): init")
-        cacheStore = PersistenceController.init(directoryURL: Self.bundleURL)
-        var error: Error?
-        cacheStore.onReady {
-            if let err = $0 {
-                error = err
-                XCTFail("Error opening \(Self.bundleURL): \(err)")
-            }
-            e.fulfill()
-        }
-        wait(for: [e], timeout: 10.0)
-        if let error = error { throw error }        
-        var entries: [CachedCarbObject]!
-        let e0 = expectation(description: "\(#function): fetch")
-        cacheStore.managedObjectContext.performAndWait {
-            do {
-                entries = try cacheStore.managedObjectContext.fetch(CachedCarbObject.fetchRequest())
-            } catch let err {
-                error = err
-            }
-            e0.fulfill()
-        }
-        wait(for: [e0], timeout: 1.0)
-        if let error = error { throw error }
-        XCTAssertEqual(1, entries.count)
-        
-        // Do some spot checks.
-        XCTAssertTrue(entityHasAttribute(entityName: "CachedGlucoseObject", attributeName: "device"))
-        XCTAssertTrue(entityHasAttribute(entityName: "CachedGlucoseObject", attributeName: "trend"))
-    }
-    
-    // TODO add func testV4toV5Migration()
-    
-    func entityHasAttribute(entityName: String, attributeName: String) -> Bool {
-        XCTAssertNotNil(cacheStore.managedObjectContext.persistentStoreCoordinator?.managedObjectModel.entities)
-        if let entities = cacheStore.managedObjectContext.persistentStoreCoordinator?.managedObjectModel.entities {
-            for entity in entities {
-                if entity.name == entityName, entity.attributesByName.contains(where: { (key: String, value: NSAttributeDescription) in
-                    key == attributeName
-                }) {
-                    return true
-                }
-            }
-        }
-        return false
     }
 }
