3072f396e26eadea62ede81fad764385fdb8ac73
Cameron Ingham <camji555@gmail.com>
2026-06-16 12:58:08 -0700

    Update for Xcode 27's @State Macro changes

--- LoopKitUI/Views/Presets/CreatePresetNameAndScheduledEdit.swift
+++ LoopKitUI/Views/Presets/CreatePresetNameAndScheduledEdit.swift
@@ -38,7 +38,7 @@ struct CreatePresetNameAndScheduledEdit: View {
     @Binding var preset: NewCustomPreset
     @Binding var path: NavigationPath
     
-    @State private var isDurationPickerExpanded = false
+    @State private var isDurationPickerExpanded: Bool
 
     @FocusState private var isTextFieldFocused: Bool
 
@@ -55,9 +55,9 @@ struct CreatePresetNameAndScheduledEdit: View {
     ) {
         self._preset = preset
         self._path = path
+        self.isDurationPickerExpanded = isDurationPickerExpanded
         self.selectedRepeatOption = preset.wrappedValue.repeatOptions == .none ? .never : .weekly
         self.onCancel = onCancel
-        self.isDurationPickerExpanded = isDurationPickerExpanded
     }
 
     var body: some View {
